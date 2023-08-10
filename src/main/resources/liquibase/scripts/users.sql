259ba4c6560c3ec2d0f89e57ab5cd016b9c12e2f
marg011 <96010371+marg011@users.noreply.github.com>
2023-08-10 15:33:10 +0400

    Update users.sql

--- src/main/resources/liquibase/scripts/users.sql
+++ src/main/resources/liquibase/scripts/users.sql
@@ -1,7 +1,7 @@
 -- liquibase formatted sql
 
 -- changeset margo:1
-CREATE  INDEX student_name_index ON users (name);
+CREATE  INDEX student_name_index ON student (name);
 
 -- changeset margo:2
 CREATE INDEX faculty_name_color ON faculty (name, color);
